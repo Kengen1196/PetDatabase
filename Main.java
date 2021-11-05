952269f917430ee87f3567ce1f72525ecbb81a70
Kengen1196 <93757686+Kengen1196@users.noreply.github.com>
2021-11-05 15:57:42 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Pet Database Program");
         // Variables used throughout the program
         Scanner input = new Scanner(System.in);
         PetDatabase database = new PetDatabase();
@@ -11,13 +12,14 @@ public class Main {
         String[] line;
         String name;
         int age;
+        int id;
         // Opening message
         System.out.println("Pet Database Program");
         // Loops until it is changed
         while (loop) {
             // Menu options
             System.out.println(
-                    "What would you like to do?\n1) View all pets\n2) Add more pets\n3)Update an exitsing pet\n4) Remove an existing pet\n5) Search pets by name\n6) Search pets by age\n7) Exit program");
+                    "What would you like to do?\n1) View all pets\n2) Add more pets\n3) Update an exitsing pet\n4) Remove an existing pet\n5) Search pets by name\n6) Search pets by age\n7) Exit program");
             // Allows user to easily see their input
             System.out.print("Your Choice: ");
             // Used in the switch
@@ -37,6 +39,25 @@ public class Main {
                 age = Integer.valueOf(line[1]);
                 database.addPets(name, age);
                 break;
+            // Updates pet in array
+            case 3:
+                System.out.print("Enter the pet ID you want to update: ");
+                id = input.nextInt();
+                input.nextLine();
+                System.out.print("Enter new name and new age: ");
+                // split input line by space as it is assumed both are given
+                line = input.nextLine().split(" ");
+                name = line[0];
+                age = Integer.valueOf(line[1]);
+                database.updatePet(id, name, age);
+                break;
+            // Removes pet in array
+            case 4:
+                System.out.print("Enter the pet ID you want to remove: ");
+                id = input.nextInt();
+                input.nextLine();
+                database.removePet(id);
+                // Search pet by name
             case 5:
                 System.out.print("Enter a name to search: ");
                 key = input.nextLine().toLowerCase();
@@ -51,6 +72,7 @@ public class Main {
             // Exits program
             case 7:
                 loop = false;
+                break;
             }
         }
         input.close();
